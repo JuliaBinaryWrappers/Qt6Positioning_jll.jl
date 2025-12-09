c0eeeb8b7c951b08251ca1859b0e082de57de941
jlbuild <juliabuildbot@gmail.com>
2025-12-09 12:13:24 +0000

    Qt6Positioning_jll build 6.8.2+0

--- src/Qt6Positioning_jll.jl
+++ src/Qt6Positioning_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qt6Positioning")
-JLLWrappers.@generate_main_file("Qt6Positioning", UUID("3ab57c46-54b1-52cd-a3f5-dc477084d9ef"))
+JLLWrappers.@generate_main_file("Qt6Positioning", Base.UUID("3ab57c46-54b1-52cd-a3f5-dc477084d9ef"))
 end  # module Qt6Positioning_jll
